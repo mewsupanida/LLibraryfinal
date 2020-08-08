7c148c0162d2d4b0c588936de17cec7d5b716898
mewsupanida <supanida.rs@gmail.com>
2020-08-08 13:47:23 +0700

    gitpod setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  #gitpod
+  config.hosts << "3000-b01a9f84-a971-4bc5-88d0-6d15006de562.ws-us02.gitpod.io"
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
